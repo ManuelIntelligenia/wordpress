1cc1b69bbe3bb7b05aeb687250ddc63f446c8156
Manuel Osorio <manuel.osorio@intelligenia.com>
2021-10-28 14:30:52 -0500

    changes

--- wordpress-plugin-observatory.js
+++ wordpress-plugin-observatory.js
@@ -1,46 +1,41 @@
-var buttonAdd = '<button type="button" id="@@" style="background-color: #008cb7!important; box-shadow: 0 3px 1px -2px #0003, 0 2px 2px 0 #00000024, 0 1px 5px 0 #0000001f; color: #fff; line-height: 36px; padding: 0 16px; margin: 5px; font-size: 12px;"> ADD TO OBSERVATORY </button>';
-var buttonEdit = '<button type="button" id="@@" style="background-color: #008cb7!important; box-shadow: 0 3px 1px -2px #0003, 0 2px 2px 0 #00000024, 0 1px 5px 0 #0000001f; color: #fff; line-height: 36px; padding: 0 16px; margin: 5px; font-size: 12px;"> EDIT OBSERVATORY METADATA </button>';
-var buttonDelete = '<button type="button" id="@@" style="background-color: #f44336!important; box-shadow: 0 3px 1px -2px #0003, 0 2px 2px 0 #00000024, 0 1px 5px 0 #0000001f; color: #fff; line-height: 36px; padding: 0 16px; margin: 5px; font-size: 12px;"> DELETE FROM OBSERVATORY </button>';
-var buttonOpen = '<button type="button" id="@@" style="background-color: #008cb7!important; box-shadow: 0 3px 1px -2px #0003, 0 2px 2px 0 #00000024, 0 1px 5px 0 #0000001f; color: #fff; line-height: 36px; padding: 0 16px; margin: 5px; font-size: 12px;"> OPEN OBSERVATORY </button>';
+var buttonAdd = '<a class="private-area-admin-button edit-file" href="@LINK@" target="_blank">ADD TO OBSERVATORY </a>';
+var buttonEdit = '<a class="private-area-admin-button edit-file" href="@LINK@" target="_blank">EDIT OBSERVATORY METADATA </a>';
+var buttonDelete = '<a class="private-area-admin-button delete-file" href="@LINK@" target="_blank">DELETE FROM OBSERVATORY</a>';
+var buttonOpen = '<a class="private-area-admin-button open-file" href="@LINK@" target="_blank">OPEN OBSERVATORY</a>';
+var apiURL = 'http://localhost:4200';
+var domainURL = 'http://localhost:4200';
 
 function getDocument() {
     console.log('Holaaaa')
-    // alert('HOLAAA');
-    // const div = url.divid;
-    // const link = url.link;
-    // const url = `http://localhost:4200/api/documents/?link=${link}&limit=1&offset=0`;
-    // const token = localStorage.getItem('ngx_token');
-    // const http = new XMLHttpRequest();
-    // http.open("GET", url);
-    // http.setRequestHeader('Authorization', `token ${token.replace('"', '').replace('"', '')}`);
-    // http.onreadystatechange = function () {
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         const resultado = JSON.parse(this.responseText)
-    //         $(`#${div}`).html(() => {
-    //             return resultado.count > 0 ? `${buttonEdit}${buttonDelete}${buttonOpen}` : `${buttonAdd}`;
-    //         });
-    //     }
-    // }
-    // http.send();
-}
-
-function getUrlAddDocument() {
-    return `http://localhost:4200/apps/document?source=private_area&visibility=private&name=${document.name}&date=${document.date}&link=${document.link}`;
-}
-
-function editDocument(button) {
-    var documentId = button.id.split('-')[2];
-    window.open(`http://localhost:4200/apps/document/${documentId}`, '_blank');
-}
-
-function deleteDocument(button) {
-    var documentId = button.id.split('-')[2];
-    window.open(`http://localhost:4200/apps/document-detail/${documentId}?delete=true`, '_blank');
-}
-
-function openDocument(button) {
-    var documentId = button.id.split('-')[2];
-    window.open(`http://localhost:4200/apps/document-detail/${documentId}`, '_blank');
+    const containerButtons = $('.private-area-row .private-area-admin-buttons div[id]');
+    for (let index = 0; index < containerButtons.length; index++) {
+        const containerButton = containerButtons[index];
+        const documentId = containerButton.getAttribute('id');
+        const documentLink = containerButton.getAttribute('data-url');
+        const documentName = containerButton.getAttribute('data-url');
+        const documentDate = containerButton.getAttribute('data-url');
+        const http = new XMLHttpRequest();
+        http.open("GET", `${apiURL}/api/documents/?link=${documentLink}&limit=1&offset=0`);
+        http.onreadystatechange = function () {
+            switch (this.status) {
+                case 200:
+                    buttonEdit = buttonEdit.replace('@LINK@', `${apiURL}/apps/document/${documentId}`);
+                    buttonDelete = buttonEdit.replace('@LINK@', `${apiURL}/apps/document-detail/${documentId}?delete=true`);
+                    buttonOpen = buttonEdit.replace('@LINK@', `${apiURL}/apps/document-detail/${documentId}`);
+                    $(`#${documentId}`).html(() => {
+                        return `${buttonEdit}${buttonDelete}${buttonOpen}`;
+                    });
+                    break;
+                case 404:
+                    buttonAdd = buttonAdd.replace('@LINK@', `${apiURL}/apps/document?source=private_area&visibility=private&name=${documentName}&date=${documentDate}&link=${documentLink}`);
+                    $(`#${documentId}`).html(() => {
+                        return `${buttonAdd}`;
+                    });
+                    break;
+            }
+        }
+        http.send();
+    }
 }
 
 getDocument();
