e7f6d6578f0ecef8f86f1ddef2b3e90c4784e4a1
Manuel Osorio <manuel.osorio@intelligenia.com>
2021-10-28 11:23:06 -0500

    fix

--- wordpress-plugin-observatory.js
+++ wordpress-plugin-observatory.js
@@ -3,9 +3,6 @@ var buttonEdit = '<button type="button" id="edit-button-1" style="background-col
 var buttonDelete = '<button type="button" id="delete-button-1" style="background-color: #f44336!important; box-shadow: 0 3px 1px -2px #0003, 0 2px 2px 0 #00000024, 0 1px 5px 0 #0000001f; color: #fff; line-height: 36px; padding: 0 16px; margin: 5px; font-size: 12px;" onClick="deleteDocument(this)"> DELETE FROM OBSERVATORY </button>';
 var buttonOpen = '<button type="button" id="open-button-1" style="background-color: #008cb7!important; box-shadow: 0 3px 1px -2px #0003, 0 2px 2px 0 #00000024, 0 1px 5px 0 #0000001f; color: #fff; line-height: 36px; padding: 0 16px; margin: 5px; font-size: 12px;" onClick="openDocument(this)"> OPEN OBSERVATORY </button>';
 
-(function () {
-})(getDocument());
-
 function getDocument() {
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('name');
@@ -46,3 +43,5 @@ function openDocument(button) {
     var documentId = button.id.split('-')[2];
     window.open(`http://localhost:4200/apps/document-detail/${documentId}`, '_blank');
 }
+
+getDocument();
